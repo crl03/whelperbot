71b476b4748a1921c0ebb62118238670ae2f0762
crl03 <clojac03@gmail.com>
2022-10-28 14:32:06 -0400

    Code cleanup.

--- src/main/java/com/bighealsinc/whelperbot/commands/LoggingChannels.java
+++ src/main/java/com/bighealsinc/whelperbot/commands/LoggingChannels.java
@@ -3,20 +3,8 @@ package com.bighealsinc.whelperbot.commands;
 import com.bighealsinc.whelperbot.listeners.CommandListener;
 import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
-import discord4j.core.object.PermissionOverwrite;
-import discord4j.core.object.entity.channel.Category;
-import discord4j.core.object.entity.channel.Channel;
-import discord4j.core.object.entity.channel.GuildChannel;
-import discord4j.core.util.EntityUtil;
-import discord4j.rest.util.Permission;
-import discord4j.rest.util.PermissionSet;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 @Component
 public class LoggingChannels extends CommandListener implements Command {
@@ -40,10 +28,6 @@ public class LoggingChannels extends CommandListener implements Command {
             event.getClient().getGuildById(Snowflake.of(1028801665492602880L))
                     .flatMap(guild -> guild.createTextChannel("member-join")
                                     .withParentId(readOnlyChannels)
-//                            .withPermissionOverwrites(PermissionOverwrite.forRole(
-//                                    guild.getEveryoneRole().block().getId(),
-//                                    PermissionSet.none(),
-//                                    PermissionSet.of(Permission.SEND_MESSAGES, Permission.ADD_REACTIONS)))
                     ).subscribe();
         }
 
@@ -51,10 +35,6 @@ public class LoggingChannels extends CommandListener implements Command {
             event.getClient().getGuildById(Snowflake.of(1028801665492602880L))
                     .flatMap(guild -> guild.createTextChannel("member-departures")
                                     .withParentId(readOnlyChannels)
-//                            .withPermissionOverwrites(PermissionOverwrite.forRole(
-//                                    guild.getEveryoneRole().block().getId(),
-//                                    PermissionSet.none(),
-//                                    PermissionSet.of(Permission.SEND_MESSAGES, Permission.ADD_REACTIONS)))
                     ).subscribe();
 
         }
